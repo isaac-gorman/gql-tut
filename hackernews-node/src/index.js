596c56b7a62addf70290806d7cf8b96f056391d9
Isaac Gorman <emailme@isaacg.net>
2020-11-10 07:42:43 -0600

    prog9

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,33 +1,24 @@
-const {GraphQLServer} = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
-let links = [
-    {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL'
-    },
-    {
-        id: 'link-1',
-        url: 'www.web.com',
-        description: 'web stuff'
-    },
 
-]
+const prisma = new PrismaClient()
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
   
-
+  // 1
   let idCount = links.length
   const resolvers = {
     Query: {
       info: () => `This is the API of a Hackernews Clone`,
       feed: () => links,
-      link: (parent, {id}) => {
-         let link = links.find(i =>  i.id == id)
-         console.log("link:", link)
-         console.log("id:", id)
-         return link
-        }
     },
     Mutation: {
+      // 2
       post: (parent, args) => {
          const link = {
           id: `link-${idCount++}`,
@@ -35,40 +26,16 @@ let links = [
           url: args.url,
         }
         links.push(link)
-        console.log("idCount:", idCount)
         return link
-      },
-      updateLink: (parent, args) => {
-        let updatedLink;
-        
-        console.log("links:", links)
-        links = links.map(link => {
-          if (link.id === args.id) {
-              console.log("link:", link)
-              console.log("args:", args)
-            updatedLink = { ...link, ...args };
-            return updatedLink;
-          }
-          return link;
-        });
-        
-        console.log("updatedLink:", updatedLink)
-        return updatedLink;
-      },
-      deleteLink: (parent, args) => {
-        const removeIndex = links.findIndex( link => link.id === args.id)
-        const removedLink = links[removeIndex]
-        links.splice(removeIndex, 1)
-        console.log("removeIndex:", removeIndex  )
-        console.log("removedLink:", removedLink  )
-        return removedLink
       }
     },
   }
-  
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers
-})
 
-server.start(() => console.log(`The server is running on http://localhost:4000 `))
+  const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: {
+      prisma,
+    }
+  })
+server.start(() => console.log(`Server is running on http://localhost:4000`))
--- hackernews-node/src/script.js
+++ hackernews-node/src/script.js
@@ -13,7 +13,6 @@ async function main() {
           url: "www.webiste.com"
       },
   })
-
   const allLinks = await prisma.link.findMany()
   console.log(allLinks)
 }
