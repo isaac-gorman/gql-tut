1eb15b05b66daad636f0532ea3f019792fe42032
Isaac Gorman <emailme@isaacg.net>
2020-11-10 09:11:18 -0600

    prog10

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -4,29 +4,23 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-  
-  // 1
-  let idCount = links.length
+
   const resolvers = {
     Query: {
       info: () => `This is the API of a Hackernews Clone`,
-      feed: () => links,
+      feed: async (parent, args, context) => {
+        return context.prisma.link.findMany()
+      },
     },
     Mutation: {
-      // 2
-      post: (parent, args) => {
-         const link = {
-          id: `link-${idCount++}`,
-          description: args.description,
-          url: args.url,
-        }
-        links.push(link)
-        return link
+      post: (parent, args, contex, info) => {
+        const newLink = contex.prisma.link.create({
+          data: {
+            url: args.url,
+            description: args.description
+          },
+        })
+        return newLink
       }
     },
   }
